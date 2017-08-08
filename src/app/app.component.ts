7d39acbf080168713560947b136ac3b224778139
Jane Shin <jane.shin@prnewswire.com>
2017-08-08 14:34:13 -0400

    1 Show the hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
 })
 export class AppComponent {
-  title = 'My First Angular App';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm'
 }
