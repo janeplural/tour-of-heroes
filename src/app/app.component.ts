31c2a6aa0d8476c5cd7ebf08b196d0d370a6267d
Jane Shin <jane.shin@prnewswire.com>
2017-08-08 15:02:40 -0400

    1 Adding HTML multi-line template strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,14 @@ export class Hero {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  `
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
