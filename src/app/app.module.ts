7ac77bc0afaf90fb82006e5bacaf9269be870ba9
Jane Shin <jane.shin@prnewswire.com>
2017-08-10 15:56:06 -0400

    redirect root to dashboard and add to main nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent} from './hero-detail.component';
 import { HeroesComponent} from './heroes.component';
 import { HeroService} from './hero.service';
@@ -13,16 +14,26 @@ import { HeroService} from './hero.service';
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-    {
-      path: 'heroes',
-      component: HeroesComponent
-    }
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
     ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
   ],
   providers: [
     HeroService
