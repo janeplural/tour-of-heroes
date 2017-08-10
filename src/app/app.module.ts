c64421a1f9c9adbe5c2033c64cbcb826ee299511
Jane Shin <jane.shin@prnewswire.com>
2017-08-10 15:36:21 -0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent} from './hero-detail.component';
+import { HeroesComponent} from './heroes.component';
+import { HeroService} from './hero.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
