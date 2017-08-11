ad82f4384ed33e1693c1bd4c3334288da4cfbaf7
Jane Shin <jane.shin@prnewswire.com>
2017-08-11 15:16:14 -0400

    extract routing related code to dedicated  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }       from './app.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent}     from './heroes.component';
 import { HeroDetailComponent} from './hero-detail.component';
-import { HeroesComponent} from './heroes.component';
-import { HeroService} from './hero.service';
+import { HeroService}         from './hero.service';
+
+import { AppRoutingModule }   from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
+    HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
